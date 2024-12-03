0d5ecc6b03e4edcc744d95571c47a7d1c244fa86
mathie <mathguy32@gmail.com>
2024-12-03 12:25:04 +0100

    improved comments

--- thread/thread.asm
+++ thread/thread.asm
@@ -5,23 +5,27 @@ incsrc "wrap.asm"
 incsrc "command.asm"
 incsrc "state.asm"
 
-process:
-    ; processes current thread.
+run:
+    ; run current thread.
     ; ----------------
-    lda !textie_thread_wait     ; wait?
-    beq +                       ; if yes,
-    dec                         ; decrease timer,
-    sta !textie_thread_wait     ;
-    rtl                         ; and return.
-    +                           ;
-    phb                         ; adjust bank.
-    phk                         ;
-    plb                         ;
-    lda !textie_thread_state    ; get state,
-    asl                         ; as a pointer,
-    tax                         ; into x,
-    jsr (state_list,x)          ; and process.
-    plb                         ; restore bank.
-    rtl                         ; end.
+
+    ; wait if thread paused
+    lda !textie_thread_wait
+    beq +
+    dec
+    sta !textie_thread_wait
+    rtl
+    +
+
+    phb
+    phk
+    plb
+    lda !textie_thread_state
+    asl
+    tax
+    jsr (state_list,x)
+    plb
+    
+    rtl
 
 namespace off
